cf106881b7eee1c3e319fc0b88af2bdd5e721b83
Izabelleticia <izabel.leticia.rocha@gmail.com>
2022-11-01 20:28:47 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,56 @@
+import collections from './data/collections.json' assert {type: 'jason'}
+import playlists from './data/playlists.json'  assert {type:'json'}
+
+var div = document.createElement('div')
+div.className = 'collection-litle'
+ 
+   var h3= document.createElement('h3')
+   h3.innerHTML = 'Spotify Playlists'
+   div.appendChild(h3)
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+    <div class="collection-title">
+        <h3>Spotify Playlists</h3>
+        <h5>VER TUDO</h5>
+    </div>
+
+    <div class="collection-cards">
+
+        PLAYLISTS
+
+    </div>
+
+*/
